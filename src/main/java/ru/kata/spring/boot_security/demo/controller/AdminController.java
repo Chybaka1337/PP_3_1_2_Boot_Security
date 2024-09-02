a052a10df2298b1c8c461e6b8990efeb526f3240
Andrey Zommer <zommer.02@mail.ru>
2024-09-03 00:21:41 +0300

    add admin controller

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,57 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController extends AbstractController {
+
+    public AdminController(UserService userService, RoleService roleService) {
+        super(userService, roleService);
+    }
+
+    @GetMapping()
+    public ModelAndView adminPage(ModelAndView mav) {
+        mav.setViewName("admin");
+        mav.addObject("users", getUserService().getAll());
+        return mav;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView saveUser() {
+        return new ModelAndView("saveUser")
+                .addObject("user", new User())
+                .addObject("roles", getRoleService().getAll());
+    }
+
+    @PostMapping("/create")
+    public String saveUser(@ModelAttribute("user") User user) {
+        getUserService().save(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/user/{id}")
+    public ModelAndView updateUser(@PathVariable int id) {
+        User user = getUserService().getUserById(id);
+        return new ModelAndView("updateUser")
+                .addObject("user", user)
+                .addObject("roles", getRoleService().getAll());
+    }
+
+    @PostMapping("/user/{id}")
+    public String updateUser(@PathVariable int id, @ModelAttribute("user") User user) {
+        getUserService().update(id, user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable int id) {
+        getUserService().delete(getUserService().getUserById(id));
+        return "redirect:/admin";
+    }
+}
