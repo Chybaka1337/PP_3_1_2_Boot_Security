0313146b17dde7c236e80e65a4209fdb8f2b4134
Andrey Zommer <zommer.02@mail.ru>
2024-09-02 19:23:53 +0300

    add the signature of the update method

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -3,12 +3,12 @@ package ru.kata.spring.boot_security.demo.services;
 import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
     User getUserById(int id);
     User getUserByName(String name);
     List<User> getAll();
     void save(User user);
+    void update(int id, User user);
     void delete(User user);
 }
