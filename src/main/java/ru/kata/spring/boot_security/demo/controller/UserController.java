262887f5004e713fbd351ee9f662a245f70e4495
Andrey Zommer <zommer.02@mail.ru>
2024-09-03 00:23:38 +0300

    shared the logic of admin and user

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -11,23 +11,14 @@ import ru.kata.spring.boot_security.demo.services.UserService;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/")
-public class UserController {
-    private final UserService userService;
-    private final RoleService roleService;
+@RequestMapping("/user")
+public class UserController extends AbstractController {
 
-    @Autowired
     public UserController(UserService userService, RoleService roleService) {
-        this.userService = userService;
-        this.roleService = roleService;
+        super(userService, roleService);
     }
 
-    @GetMapping("/index")
-    public String getIndex() {
-        return "index";
-    }
-
-    @GetMapping("/user")
+    @GetMapping()
     public ModelAndView getUser(HttpSession http, ModelAndView mav) {
         User user = (User) http.getAttribute("user");
         mav
@@ -35,46 +26,5 @@ public class UserController {
                 .addObject("roles", user.getRoles())
                 .setViewName("user");
         return mav;
-    }
-
-    @GetMapping("/admin")
-    public ModelAndView adminPage(ModelAndView mav) {
-        mav.setViewName("admin");
-        mav.addObject("users", userService.getAll());
-        return mav;
-    }
-
-    @GetMapping("/admin/create")
-    public ModelAndView saveUser() {
-        return new ModelAndView("saveUser")
-                .addObject("user", new User())
-                .addObject("roles", roleService.getAll());
-    }
-
-    @PostMapping("/admin/create")
-    public String saveUser(@ModelAttribute("user") User user) {
-        userService.save(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/admin/user/{id}")
-    public ModelAndView updateUser(@PathVariable int id) {
-        User user = userService.getUserById(id);
-        return new ModelAndView("updateUser")
-                .addObject("user", user)
-                .addObject("roles", roleService.getAll());
-    }
-
-    //TODO
-    @PostMapping("/admin/user/{id}")
-    public String updateUser(@PathVariable int id, @ModelAttribute("user") User user) {
-        userService.update(id, user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/admin/user/delete/{id}")
-    public String deleteUser(@PathVariable int id) {
-        userService.delete(userService.getUserById(id));
-        return "redirect:/admin";
     }
 }
