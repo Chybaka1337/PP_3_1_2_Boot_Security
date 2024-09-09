84b7e2ba1cb63df9ab7e461344de9ad88bea5583
Andrey Zommer <zommer.02@mail.ru>
2024-09-09 23:25:02 +0300

    change getPage method

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AbstractController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AbstractController.java
@@ -2,8 +2,14 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.services.RoleService;
 import ru.kata.spring.boot_security.demo.services.UserService;
+
+import javax.servlet.http.HttpSession;
+import java.util.stream.Collectors;
 
 @Controller
 public abstract class AbstractController {
@@ -14,6 +20,16 @@ public abstract class AbstractController {
     public AbstractController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
+    }
+
+    public ModelAndView getPage(HttpSession http, ModelAndView mav) {
+        User user = (User) http.getAttribute("user");
+        mav.addObject("cur_user", user);
+        mav.addObject(
+                "roles",
+                user.getRoles().stream().map(Role::getRoleName).collect(Collectors.toSet())
+        );
+        return mav;
     }
 
     public UserService getUserService() {
